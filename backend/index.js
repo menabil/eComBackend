285d195ead59093f8f38e342287cf13203eebd72
menabil <nabil99cc@gmail.com>
2026-09-18 23:37:11 +0600

    new index file

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log(` Server is running on port: ${port}`);
+});
